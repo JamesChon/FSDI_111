abad08afc120986aaaec95385781ab4fc34f286e
Chon <james.chon@sdgku.edugit config --global user.name Chongit config --global user.email james.chon@sdgku.edu>
2024-04-17 17:44:37 -0700

    First commit

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from flask import Flask       # from the flask module import the Flask class
+
+app = Flask(__name__)         # Create an instance of the Flask class
+
+@app.get("/about")
+@app.get("/")                 # Flask decorator that maps view functions to routes  
+def index():                  # view function
+    me = {                    # python dictionary  
+        "first_name": "James",
+        "last_name": "Chon",
+        "hobbies": "Gaming",
+        "is_online": True
+    }
+    return me                 # When you return a dict from a view function, it becomes a JSON  
+
+ 
